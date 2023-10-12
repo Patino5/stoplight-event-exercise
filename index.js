5e0c45e2e95c6caa37df2623d955108af4662b0e
Patino5 <106916823+Patino5@users.noreply.github.com>
2023-10-13 08:14:38 +1000

    Toggled lights with the buttons

--- index.js
+++ index.js
@@ -2,4 +2,27 @@
   'use strict';
 
   // YOUR CODE HERE
+  // declare btn var for each button
+  const stopBtn = document.querySelector('#stopButton')
+  const slowBtn = document.querySelector('#slowButton')
+  const goBtn = document.querySelector('#goButton')
+
+  // declare light var for each light
+  let stopLight = document.querySelector('#stopLight')
+  let slowLight = document.querySelector('#slowLight')
+  let goLight = document.querySelector('#goLight')
+
+  // add eventListener for on click to add/remove class name to stop to stopLight
+  stopBtn.addEventListener('click', function(){
+    stopLight.classList.toggle('stop')
+  })
+
+  slowBtn.addEventListener('click', function(){
+    slowLight.classList.toggle('slow')
+  })
+
+  goBtn.addEventListener('click', function (){
+    goLight.classList.toggle('go')
+  })
+
 })();
